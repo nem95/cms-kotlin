d56f405782de8dde41fb3de5c2ad831ea6cdda19
Naïm <naiim.chatti@gmail.com>
2019-02-22 09:56:35 +0100

    add Fake model

--- src/test/kotlin/fr/iim/iwm/a5/chatti/naim/fakeModel.kt
+++ src/test/kotlin/fr/iim/iwm/a5/chatti/naim/fakeModel.kt
@@ -1,45 +1,15 @@
 package fr.iim.iwm.a5.chatti.naim
 
+
 class FakeModel : Model {
-    val connectionPool = ConnectionPool("jdbc:mysql://localhost:6000/cms", "root", "root")
-
     override fun getArticleList(): List<Article> {
-        val articles = ArrayList<Article>()
-        connectionPool.use { connection ->
-            connection.prepareStatement("SELECT * FROM articles").use { stmt ->
-                val results = stmt.executeQuery()
-
-
-                while (results.next()) {
-                    articles += Article(results.getInt("id"), results.getString("title"), results.getString("text"))
-                }
-            }
-        }
-        return articles
-
+        return listOf(Article(42, "Super Titre"))
     }
 
     override fun getArticle(id: Int): Article? {
-        val articles = ArrayList<Article>()
-
-        connectionPool.use { connection ->
-            connection.prepareStatement("SELECT * FROM articles WHERE id = ?").use { stmt ->
-                stmt.setInt(1, id)
-
-                val results = stmt.executeQuery()
-                val found = results.next()
-
-
-                if (found) {
-                    return Article(
-                        results.getInt("id"),
-                        results.getString("title"),
-                        results.getString("text")
-                    )
-                }
-            }
-        }
-
-        return null
+        return if (id === 42)
+            Article(42, "Title", "Text value")
+        else
+            null
     }
 }
